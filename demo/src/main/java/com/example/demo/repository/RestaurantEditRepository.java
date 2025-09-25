9120bb17e3c332ac946aa69c883e5e53b5249788
Kota_Mukai <kota1022@mukai@gmail.com>
2025-09-25 15:07:39 +0900

    Coded three interfaces and fixed some mistakes in RestaurantSearchResult.

---
+++ demo/src/main/java/com/example/demo/repository/RestaurantEditRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.RestaurantInformation;
+
+public interface RestaurantEditRepository {
+
+    void update(RestaurantInformation restaurantInformation);
+
+}
