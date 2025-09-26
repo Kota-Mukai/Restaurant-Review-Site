ef84710d057344d9ab1a6cc1151d0310b0b8dd92
Kota_Mukai <kota1022@mukai@gmail.com>
2025-09-26 12:07:19 +0900

    Coded Service interfaces that use RestaurantInformation...

---
+++ demo/src/main/java/com/example/demo/service/RestaurantDeleteService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.RestaurantInformation;
+
+public interface RestaurantDeleteService {
+
+    void delete(RestaurantInformation restaurantInformation);
+
+}
---
+++ demo/src/main/java/com/example/demo/service/RestaurantEditService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.RestaurantInformation;
+
+public interface RestaurantEditService {
+
+    void edit(RestaurantInformation restaurantInformation);
+
+}
