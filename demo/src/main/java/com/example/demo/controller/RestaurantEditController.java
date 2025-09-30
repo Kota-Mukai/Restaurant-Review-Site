52c173caae8d571a9f6ae60760e67760a2adedb6
Kota_Mukai <kota1022@mukai@gmail.com>
2025-09-30 11:36:51 +0900

    pull request #20

---
+++ demo/src/main/java/com/example/demo/controller/RestaurantEditController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.entity.RestaurantInformation;
+import com.example.demo.form.RestaurantUpdateForm;
+import com.example.demo.service.RestaurantEditService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class RestaurantEditController {
+
+    private final RestaurantEditService service;
+
+    @PostMapping("/show-edit-restaurant-form")
+    public String showEditForm(@ModelAttribute RestaurantUpdateForm form) {
+
+        return "edit-restaurant";
+    }
+
+    @PostMapping("/edit-restaurant")
+    public String editRestaurant(
+            @Validated @ModelAttribute RestaurantUpdateForm form,
+            BindingResult result,
+            RedirectAttributes redirectAttributes){
+
+        if(result.hasErrors()){
+            return "edit-restaurant";
+        }
+
+        RestaurantInformation re = new RestaurantInformation();
+        re.setRestaurantId(form.getRestaurantId());
+        re.setRestaurantName(form.getRestaurantName());
+        re.setTagline(form.getTagline());
+
+        service.edit(re);
+
+        redirectAttributes.addFlashAttribute("msg" , "Edit restaurnat information");
+
+        return "redirect:/complete";
+
+    }
+    
+}
