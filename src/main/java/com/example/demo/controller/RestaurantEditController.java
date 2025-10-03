4b0d6b334f0c13443ee7413de2b4de07885e1664
Kota_Mukai <kota1022@mukai@gmail.com>
2025-10-03 09:56:22 +0900

    pull request #32

--- src/main/java/com/example/demo/controller/RestaurantEditController.java
+++ src/main/java/com/example/demo/controller/RestaurantEditController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import javax.naming.Binding;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
